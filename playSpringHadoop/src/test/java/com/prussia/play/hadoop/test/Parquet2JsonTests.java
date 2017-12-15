9458015b7ed9c3cd734cb87a665fdfc18952f3ee
Prussia <prussia.hu@pwc.com>
2017-12-15 16:02:38 +0800

    calculate the row count of parquet folder

--- playSpringHadoop/src/test/java/com/prussia/play/hadoop/test/Parquet2JsonTests.java
+++ playSpringHadoop/src/test/java/com/prussia/play/hadoop/test/Parquet2JsonTests.java
@@ -1,9 +1,17 @@
 package com.prussia.play.hadoop.test;
 
+import static java.util.stream.Collectors.toList;
+
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.parquet.hadoop.Footer;
 import org.apache.parquet.hadoop.ParquetFileReader;
 import org.apache.parquet.hadoop.metadata.ParquetMetadata;
 
@@ -16,12 +24,29 @@ public class Parquet2JsonTests {
 
 		System.setProperty("hadoop.home.dir", "/");
 		System.setProperty("HADOOP_USER_NAME", "hdfs");
-		URL url = Parquet2JsonTests.class.getClassLoader().getResource("new.snappy.parquet");
-		ParquetMetadata metadata = ParquetFileReader.readFooter(new Configuration(), new Path(url.getPath()));
-		
-		log.debug(metadata.toPrettyJSON(metadata));
-		 
-		 
+
+		URL url = Parquet2JsonTests.class.getClassLoader().getResource("test.parquet");
+		Configuration conf = new Configuration();
+		// FileSystem fs = FileSystem.get(conf);
+		LocalFileSystem localFileSystem = FileSystem.getLocal(conf);
+
+		Path filePath = new Path(url.getPath());
+
+		FileStatus[] fileStatus = localFileSystem.listStatus(filePath);
+		List<FileStatus> fileStatusList = Arrays.stream(fileStatus).collect(toList());
+
+		// ParquetMetadata metadata = ParquetFileReader.readFooter(new
+		// Configuration(), new Path(url.getPath()));
+
+		List<Footer> footers = ParquetFileReader.readAllFootersInParallel(conf, fileStatusList, false);
+		// footers.stream().forEach(e -> log.debug(
+		// "ParquetMetadata = {}",
+		// ParquetMetadata.toPrettyJSON(e.getParquetMetadata()))
+		// );
+		long count = footers.stream()
+				.mapToLong(e -> e.getParquetMetadata().getBlocks().stream().mapToLong(t -> t.getRowCount()).sum())
+				.sum();
+		log.warn("total count = {}", count);
 	}
 
 }
