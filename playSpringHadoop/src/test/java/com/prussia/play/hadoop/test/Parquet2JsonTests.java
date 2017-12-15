1e7ef5df84b571781d0b67559ff4afdd4ee15a17
Prussia <prussia.hu@pwc.com>
2017-12-15 16:18:47 +0800

    filter parquet file

--- playSpringHadoop/src/test/java/com/prussia/play/hadoop/test/Parquet2JsonTests.java
+++ playSpringHadoop/src/test/java/com/prussia/play/hadoop/test/Parquet2JsonTests.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
@@ -25,28 +26,29 @@ public class Parquet2JsonTests {
 		System.setProperty("hadoop.home.dir", "/");
 		System.setProperty("HADOOP_USER_NAME", "hdfs");
 
-		URL url = Parquet2JsonTests.class.getClassLoader().getResource("test.parquet");
+		URL fileURL = Parquet2JsonTests.class.getClassLoader().getResource("prussia_order.parquet");
 		Configuration conf = new Configuration();
 		// FileSystem fs = FileSystem.get(conf);
-		LocalFileSystem localFileSystem = FileSystem.getLocal(conf);
+		try (LocalFileSystem localFileSystem = FileSystem.getLocal(conf);) {
 
-		Path filePath = new Path(url.getPath());
+			Path filePath = new Path(fileURL.getPath());
 
-		FileStatus[] fileStatus = localFileSystem.listStatus(filePath);
-		List<FileStatus> fileStatusList = Arrays.stream(fileStatus).collect(toList());
+			FileStatus[] fileStatus = localFileSystem.listStatus(filePath);
+			List<FileStatus> fileStatusList = Arrays.stream(fileStatus).filter(f -> "parquet".equals(FilenameUtils.getExtension(f.getPath().getName()))).collect(toList());
 
-		// ParquetMetadata metadata = ParquetFileReader.readFooter(new
-		// Configuration(), new Path(url.getPath()));
+			// ParquetMetadata metadata = ParquetFileReader.readFooter(new
+			// Configuration(), new Path(url.getPath()));
 
-		List<Footer> footers = ParquetFileReader.readAllFootersInParallel(conf, fileStatusList, false);
-		// footers.stream().forEach(e -> log.debug(
-		// "ParquetMetadata = {}",
-		// ParquetMetadata.toPrettyJSON(e.getParquetMetadata()))
-		// );
-		long count = footers.stream()
-				.mapToLong(e -> e.getParquetMetadata().getBlocks().stream().mapToLong(t -> t.getRowCount()).sum())
-				.sum();
-		log.warn("total count = {}", count);
+			List<Footer> footers = ParquetFileReader.readAllFootersInParallel(conf, fileStatusList, false);
+			// footers.stream().forEach(e -> log.debug(
+			// "ParquetMetadata = {}",
+			// ParquetMetadata.toPrettyJSON(e.getParquetMetadata()))
+			// );
+			long count = footers.stream()
+					.mapToLong(e -> e.getParquetMetadata().getBlocks().stream().mapToLong(t -> t.getRowCount()).sum())
+					.sum();
+			log.warn("total count = {}", count);
+		}
 	}
 
 }
